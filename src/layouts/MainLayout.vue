24e9d144617dbf9446aeb4d9225533e3b2319c20
djkenneth <dj.kenneth.pineda@gmail.com>
2024-09-21 16:15:41 +0800

    update curve layer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,3 @@
-<template>
-  <q-layout view="lHh Lpr lFf" >
-    <q-header class="bg-white">
-      <q-toolbar class="flex justify-between">
-        <img
-          alt="CPA logo"
-          src="~assets/logo.svg"
-          style="width: 80px; height: 70px"
-        >
-        <div class="flex items-center icons">
-          <img
-              alt="Notification Icon"
-              src="~assets/notification-icon.svg"
-              class=""
-              style="width: 20px"
-            >
-            <img
-              alt="Dropdown Icon"
-              src="~assets/dropdown-icon.svg"
-              style="width: 15px"
-            >
-        </div>
-
-      </q-toolbar>
-    </q-header>
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-    <q-footer class="footer fixed-bottom bg-white q-px-sm q-py-sm">
-        <div class="flex justify-between">
-            <MenuIcon alt="menu profile" icon="profile.svg" title="Profile" />
-            <MenuIcon alt="menu messages" icon="messages.svg" title="Messages" />
-            <MenuIcon alt="menu home" icon="home.svg" title="" />
-            <MenuIcon alt="menu messenger" icon="messenger.svg" title="Messenger" />
-            <MenuIcon alt="menu activity" icon="activity.svg" title="Activity" />
-        </div>
-    </q-footer>
-  </q-layout>
-</template>
-
 <script setup>
 import MenuIcon from '../components/MenuIcon.vue'
 import { ref } from 'vue'
@@ -53,7 +13,42 @@ defineOptions({
 // }
 </script>
 
-<style lang="scss">
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white">
+      <q-toolbar class="flex justify-between">
+        <img alt="CPA logo" src="~assets/logo.svg" style="width: 80px; height: 70px">
+        <div class="flex items-center icons">
+          <img alt="Notification Icon" src="~assets/notification-icon.svg" class="" style="width: 20px">
+          <img alt="Dropdown Icon" src="~assets/dropdown-icon.svg" style="width: 15px">
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer class="footer fixed-bottom bg-white q-px-sm q-pb-sm">
+      <div class="flex justify-between menu-wrapper">
+        <div class="menu-layer" style="display: flex; justify-content: space-between;"></div>
+        <div class="menus">
+          <MenuIcon alt="menu profile" icon="profile.svg" title="Profile" />
+          <MenuIcon alt="menu messages" icon="messages.svg" title="Messages" />
+        </div>
+        <div class="menu-home-wrapper">
+          <MenuIcon alt="menu home" icon="home.svg" title="" />
+        </div>
+        <div class="menus">
+          <MenuIcon alt="menu messenger" icon="messenger.svg" title="Messenger" />
+          <MenuIcon alt="menu activity" icon="activity.svg" title="Activity" />
+        </div>
+
+
+      </div>
+    </q-footer>
+  </q-layout>
+</template>
+
+<style lang="scss" scoped>
 .icons {
   gap: 15px;
 }
@@ -63,6 +58,49 @@ defineOptions({
 }
 
 .footer {
-  box-shadow: 0 7px 9px -7px #087F9640 inset;
+
+  // box-shadow: 0 7px 9px -7px #087F9640 inset;
+  .menu-wrapper {
+    position: relative;
+    overflow: hidden;
+    padding-block: 3px;
+
+    .menus {
+      display: flex;
+      align-items: center;
+      gap: 25px
+    }
+
+    .menu-home-wrapper {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      top: 25px;
+    }
+
+    .menu-layer {
+      position: absolute;
+      background-image: url("layer2.svg");
+      background-repeat: no-repeat;
+      background-size: 603px;
+      width: 100%;
+      height: 100px;
+      background-position-y: bottom;
+      background-position-x: center;
+      bottom: -22px;
+    }
+  }
+}
+
+@media only screen and (max-width: 320px) {
+  .footer {
+    .menu-wrapper {
+      .menus {
+        display: flex;
+        align-items: center;
+        gap: 5px
+      }
+    }
+  }
 }
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,45 +1,3 @@
-<template>
-  <q-page clas="relative-position">
-    
-    <div class="wrapper">
-        <img
-          alt="home"
-          src="~assets/camera.svg"
-          class="home-button-wrapper"
-        >
-      <q-input rounded standout type="search" v-model="search" label="Search">
-        <template v-slot:prepend>
-          <q-icon name="menu" />
-        </template>
-        <template v-slot:append>
-          <q-icon name="search" />
-        </template>
-      </q-input>
-      <!-- <ManualCircle /> -->
-      <Circle />
-      <div class="intro">
-        <h3 class="intro-title">911 Call Emergency</h3>
-        <p class="intro-description">
-          Call emergency on the City Hotline or report crime and emergency situations in Realtime response.
-        </p>
-      </div>
-
-      <div class="profile">
-        <p class="profile-title">Profile Completion</p>
-        <div class="progress">
-          <q-linear-progress
-            size="5px"
-            :value="progress"
-            color="primary"
-            track-color="cyan"
-          />
-        </div>
-        <p class="profile-description">Complete your profile to access all Verified Citizen Privileges</p>
-      </div>
-    </div>
-  </q-page>
-</template>
-
 <script setup>
 // import ManualCircle from '../components/ManualCircle.vue'
 import Circle from '../components/Circle.vue'
@@ -52,77 +10,126 @@ defineOptions({
 const search = ref('')
 const progress = ref(0.2)
 </script>
-<style lang="scss">
-  .home-button-wrapper {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 45px;
-      height: 45px;
-      position: absolute;
-      top: -2px;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    }
 
-  .wrapper {
+<template>
+  <q-page clas="relative-position">
+    <div class="main-wrapper">
+      <div class="layer2-wrapper">
+        <div class="layer-wrapper">
+          <img alt="home" src="~assets/camera.svg" class="home-button-wrapper">
+          <div class="content">
+            <q-input rounded standout type="search" v-model="search" label="Search">
+              <template v-slot:prepend>
+                <q-icon name="menu" />
+              </template>
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+            <Circle />
+            <div class="intro">
+              <h3 class="intro-title">911 Call Emergency</h3>
+              <p class="intro-description">
+                Call emergency on the City Hotline or report crime and emergency situations in Realtime response.
+              </p>
+            </div>
+            <div class="profile">
+              <p class="profile-title">Profile Completion</p>
+              <div class="progress">
+                <q-linear-progress size="5px" :value="progress" color="primary" track-color="cyan" />
+              </div>
+              <p class="profile-description">Complete your profile to access all Verified Citizen Privileges</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </q-page>
+</template>
+
+
+<style lang="scss" scoped >
+.main-wrapper {
   position: relative;
-  padding: 3.5rem 1rem 2rem 1rem;
-  background-color: #F7FAFC;
-  margin-top: 3rem;
-  box-shadow: 0 -5px 15px 1px #087F9640;
+  width: 100%;
+  height: 100vh;
 
+  .layer2-wrapper {
+    background-image: url("layer2.svg");
+    background-repeat: no-repeat;
+    background-size: 450px;
+    width: 100%;
+    height: 100vh;
+    background-position-x: center;
+    position: relative;
 
-    &::before {
-      content: "";
+    .layer-wrapper {
       position: absolute;
-      height: 80px;
-      width: 80px;
-      background-color: #F7FAFC;
-      top: -2px;
+      top: 18px;
+      left: 0;
+      background-image: url('layer.svg');
+      background-repeat: no-repeat;
+      background-size: inherit;
+      background-position-x: center;
+      width: 100%;
+      height: inherit
+    }
+
+    .home-button-wrapper {
+      width: 35px;
+      height: 35px;
+      position: absolute;
+      top: 28px;
       left: 50%;
-      border-radius: 100%;
       transform: translate(-50%, -50%);
-      box-shadow: 0 -5px 15px 1px #087F9640;
     }
 
-    .intro {
-      &-title {
-        text-align: center;
-        font-size: 2.2rem;
-        color: #00687B;
-        margin-bottom: 18px;
+    .content {
+      position: relative;
+      padding-top: 4rem;
+      max-width: 90%;
+      margin: auto;
+
+      .intro {
+        &-title {
+          text-align: center;
+          font-size: 2.2rem;
+          color: #00687B;
+          margin-bottom: 18px;
+        }
+
+        &-description {
+          text-align: center;
+          font-size: 16px;
+        }
       }
 
-      &-description {
-        text-align: center;
-        font-size: 16px;
-      }
-    }
+      .profile {
+        padding-bottom: 5rem;
 
-    .profile {
-      &-title {
-        text-align: center;
-        font-size: 1rem;
-        font-weight: 500;
-        color: #00687B;
-      }
-      .progress {
-        display: flex;
-        align-items: center;
-        gap: 20px;
-      }
-      &-description {
-        text-align: center;
-        font-weight: 500;
-        font-size: 0.8rem;
-        padding-top: 10px;
-        color: '#000';
+        &-title {
+          text-align: center;
+          font-size: 1rem;
+          font-weight: 500;
+          color: #00687B;
+        }
+
+        .progress {
+          display: flex;
+          align-items: center;
+          gap: 20px;
+        }
+
+        &-description {
+          text-align: center;
+          font-weight: 500;
+          font-size: 0.8rem;
+          padding-top: 10px;
+          color: '#000';
+        }
       }
     }
   }
-
-
-  
+}
 </style>
-
